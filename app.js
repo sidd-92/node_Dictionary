a7d88976cc66dbc1d791704a71047387d20d92ab
sidd-92 <sidddharthvn2014@gmail.com>
2018-01-25 18:07:46 -0800

    Used routes to get post and delete term from dictionary

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+var bodyParser = require('body-parser');
 //Create a Instance of express
 var app = express();
 
@@ -18,10 +19,14 @@ var someTerms = [
     }
 ];
 
+//Use body parser Here
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended:false})); //We need to set to true only if u have large amount of nested data
+
 
 
 app.use((req, res, next) => {
-    console.log(`Requested Methods : ${req.method} | Requested URL : ${req.url}`);
+    console.log(`Req Methods : ${req.method} | ReqURL : ${req.url} - ${JSON.stringify(req.body)}`);
     //Need to invoke next function to tell it to continue
     next();
 });
@@ -38,9 +43,21 @@ app.get("/dictionary-api", (req, res) => {
     //Its similar to the http
     //but now response has a JSON function which takes in json and automatically handle data
     res.json(someTerms);
-
 });
 
+//We will add a post route
+app.post("/dictionary-api", (req,res) => {
+    someTerms.push(req.body);
+    res.json(someTerms);
+});
+
+app.delete("/dictionary-api/:term", (req, res) => {
+    //use this req.params.term to get the term u wanted to delete
+    someTerms = someTerms.filter((definition) => {
+        return definition.term.toLowerCase() !== req.params.term.toLowerCase();
+    });
+    res.json(someTerms);
+});
 //Tell the express app to listen on port 3001
 app.listen(3001);
 console.log("Server Is Running at 3001");
